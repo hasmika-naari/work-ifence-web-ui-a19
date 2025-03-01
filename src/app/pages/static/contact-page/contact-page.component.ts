cbb8e704ef86e0748aa37534303e732cc1b8df64
hasmikak <hasmika.naari@gmail.com>
2025-03-01 16:15:27 -0500

    Feedback UI changes

--- src/app/pages/static/contact-page/contact-page.component.ts
+++ src/app/pages/static/contact-page/contact-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgOptimizedImage, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID, inject } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import * as _ from 'lodash';
 import { DeviceDetectorService } from 'ngx-device-detector';
@@ -12,14 +12,23 @@ import { PCategory } from 'src/app/services/bee-compete.model';
 import { WorkifenceDataService } from 'src/app/services/bee-compete-data.service';
 import { ThemeCustomizerService } from 'src/app/services/theme-customizer/theme-customizer.service';
 import { FooterWorkifenceComponent } from '../../landing/footer-wifence/footer-wifence.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-            CommonModule, RouterLink,LanguageSubscribeComponent,
+            CommonModule, RouterLink,LanguageSubscribeComponent, MatFormFieldModule,
             ReactiveFormsModule, NgOptimizedImage, HeaderWorkIfenceComponent,
-            FooterComponent, FooterWorkifenceComponent
+            MatSelectModule, MatProgressBarModule, MatCardModule, FormsModule,
+            FooterComponent, FooterWorkifenceComponent, MatSnackBarModule,
+            MatInputModule, MatButtonModule
           ],
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss']
@@ -39,55 +48,30 @@ export class ContactComponent implements OnInit {
   isDesktop = true;
   browser = false;
 
-  public feedbackForm: FormGroup = new FormGroup({
-    fullname: new FormControl(''),
-    email: new FormControl(''),
-    phonenumber: new FormControl(''),
-    subject: new FormControl(''),
-    message: new FormControl(false),
-  });
+  loading: boolean = false;
+  requestTypes = [
+    { value: 'newOrg', label: 'Request for new Org Account' },
+    { value: 'newFeature', label: 'Request for New Feature' },
+    { value: 'loginIssue', label: 'Issue with Login' }
+  ];
+  feedbackForm: FormGroup<{ firstName: FormControl<string | null>; lastName: FormControl<string | null>; emailId: FormControl<string | null>; phoneNumber: FormControl<string | null>; requestType: FormControl<string | null>; requestDescription: FormControl<string | null>; }>;
 
-  constructor() {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
       this.browser = isPlatformBrowser(this.platformId);
       this.themeService.isToggled$.subscribe(isToggled => {
         this.isToggled = isToggled;
     });
+    this.feedbackForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailId: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      requestType: ['', Validators.required],
+      requestDescription: ['', Validators.required]
+    });
      }
 
   ngOnInit() {
-
-    this.feedbackForm = this.formBuilder.group(
-      {
-        fullname: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(30),
-          ],
-        ],
-        email: ['', [Validators.required, Validators.email]],
-        phonenumber: ['', [Validators.required]],
-        subject: [
-          '', 
-          [
-            Validators.required, 
-            Validators.minLength(10),
-            Validators.maxLength(128)
-          ]
-        ],
-        message: [
-          '', 
-          [
-            Validators.required, 
-            Validators.minLength(10),
-            Validators.maxLength(512)
-          ]
-        ],
-      }
-      
-    );
-
     this.fetchData();
     if(this.browser){
       if(this.deviceService.isDesktop()){
@@ -110,31 +94,46 @@ export class ContactComponent implements OnInit {
     return this.feedbackForm.controls;
   }
 
-  onSubmit(): void {
-    this.feedbackSubmitted = true;
-    console.log('onSubmit - 1');
-    if (this.feedbackForm.invalid) {
-      return;
+  onSubmit() {
+    if (this.feedbackForm.valid) {
+      this.loading = true; // Show progress bar
+
+      // Simulate API request
+      setTimeout(() => {
+        this.loading = false;
+        this.snackBar.open('Feedback Submitted Successfully!', 'Close', {
+          duration: 3000
+        });
+        this.feedbackForm.reset();
+      }, 2000);
     }
-    console.log('onSubmit - 2');
-
-    // let registerRequest = new RegisterRequest();
-    // registerRequest.login = this.registerForm.value.username; 
-    // registerRequest.email = this.registerForm.value.email; 
-    // registerRequest.password = this.registerForm.value.password;
-    // registerRequest.langKey = "en";
-    // this.authService.signUp(registerRequest).subscribe((resp) => {
-    //     this.activationForm.controls['username'].setValue(this.registerForm.value.username);
-    //     this.activationForm.controls['username'].disable();
-
-    //     this.showActivationForm = true;
-    // }, (error) => {
-
-    // });
-    console.log('onSubmit - 3');
-
-    console.log(JSON.stringify(this.feedbackForm.value.fullname, null, 2));
   }
+
+  // onSubmit(): void {
+  //   this.feedbackSubmitted = true;
+  //   console.log('onSubmit - 1');
+  //   if (this.feedbackForm.invalid) {
+  //     return;
+  //   }
+  //   console.log('onSubmit - 2');
+
+  //   // let registerRequest = new RegisterRequest();
+  //   // registerRequest.login = this.registerForm.value.username; 
+  //   // registerRequest.email = this.registerForm.value.email; 
+  //   // registerRequest.password = this.registerForm.value.password;
+  //   // registerRequest.langKey = "en";
+  //   // this.authService.signUp(registerRequest).subscribe((resp) => {
+  //   //     this.activationForm.controls['username'].setValue(this.registerForm.value.username);
+  //   //     this.activationForm.controls['username'].disable();
+
+  //   //     this.showActivationForm = true;
+  //   // }, (error) => {
+
+  //   // });
+  //   console.log('onSubmit - 3');
+
+  //   console.log(JSON.stringify(this.feedbackForm.value.fullname, null, 2));
+  // }
 
 fetchData(): void{
   // this.dealsService.getCategoriesByCountry('usa', this.platformId).subscribe((categories) => {
