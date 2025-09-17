f7d8c15f0d295ad7c79e3590e35b6cadaaa4b508
hasmika-naari <hasmika.naari@gmail.com>
2025-09-17 00:14:59 -0400

    sidenav fixed

--- src/app/pages/landing/header-wifence/menu-sidebar/menu-sidebar.component.ts
+++ src/app/pages/landing/header-wifence/menu-sidebar/menu-sidebar.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, Input, Output, EventEmitter, inject, ChangeDetectionStrategy } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Component, Input, Output, EventEmitter, inject, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { RouterLink, RouterModule, Router } from '@angular/router';
 import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { IconsModule } from 'src/app/shared/icons.module';
 import { MenuListItem } from '../../../../services/bee-compete.model';
+import { LocalStorageService } from '../../../../services/local-storage.service';
 
 @Component({
   selector: 'app-menu-sidebar',
@@ -24,7 +25,33 @@ import { MenuListItem } from '../../../../services/bee-compete.model';
 export class MenuSidebarComponent {
   @Input() menuSidebarOpen: boolean = false;
   @Input() menuItems: MenuListItem[] = [];
+  @Input() userAccount: any;
   @Output() closeMenuSidebar = new EventEmitter<void>();
 
+  private _localStorageService: LocalStorageService = inject(LocalStorageService);
+  private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+  private router: Router = inject(Router);
+
   constructor() {}
+  
+  /**
+   * Logout the user and redirect to home page
+   */
+  logout(): void {
+    // Clear user data from local storage
+    this._localStorageService.removeItem('user');
+    this._localStorageService.removeItem('token');
+    
+    // Close the sidebar
+    this.closeMenuSidebar.emit();
+    
+    // Reset user account
+    this.userAccount = null;
+    
+    // Navigate to home page
+    this.router.navigate(['/']);
+    
+    // Force change detection
+    this.cdr.detectChanges();
+  }
 }
